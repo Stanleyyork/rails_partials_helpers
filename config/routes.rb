0d8287c76e1aa09e62fd5f5723001862a94426c4
stanleyyork <stanleyyork@gmail.com>
2015-12-18 10:55:19 -0800

    init commit

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get "/signup", to: "users#new"
   get "/login", to: "sessions#new"
   get "/logout", to: "sessions#destroy"
+  delete "users/:id", to: "users#destroy", as: "delete_user"
+  delete "recipes/:id", to: "recipes#destroy", as: "delete_recipe"
   resources :sessions, only: [:create]
 
 end
